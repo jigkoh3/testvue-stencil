72df35b189d2f074b44d44660642068d6f474059
Theerasak Tubrit <jigkoh3@Macintoshs-MacBook-Pro.local>
2020-10-12 13:27:33 +0700

    init test stencil vue

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import {applyPolyfills, defineCustomElements} from 'thai-id-card-camera/loader'
+
 Vue.config.productionTip = false
+
+// Bind the custom elements to the window object
+applyPolyfills().then(() => {
+  defineCustomElements();
+});
 
 new Vue({
   render: h => h(App),
